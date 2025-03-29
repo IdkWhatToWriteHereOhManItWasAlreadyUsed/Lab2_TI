90e4aa642b511ffc57ea1cf9e4aea208f91276ca
Mona Lisa <dimoon.amid05@gmail.com>
2025-03-30 02:19:17 +0300

    optimised

--- Key.cpp
+++ Key.cpp
@@ -2,7 +2,7 @@
 #include <cstdint>
 #include "Key.h"
 
-
+/*
 std::vector<uint8_t> generateKey(std::vector<uint8_t>& key, int length) {
    // return generateKey(key, polynomial, length);
     return generateKeyAppending(key, polynomial, length);
@@ -10,7 +10,7 @@ std::vector<uint8_t> generateKey(std::vector<uint8_t>& key, int length) {
 
 /// корректная версия
 
-uint8_t calculateFeedbackBit(std::vector<uint8_t>& reg, const std::vector<int>& polynomial) 
+uint8_t calculateFeedbackBit(const std::vector<uint8_t>& reg, const std::vector<int>& polynomial) 
 {
     uint8_t feedback = 0;
     for (int tap : polynomial) {
@@ -37,6 +37,39 @@ std::vector<uint8_t> generateKeyAppending(std::vector<uint8_t>& initialKey,  con
     for (int i = 0; i < outputLength; ++i) {
         generatedKey[i] = initialKey[0];
         shiftLeft(initialKey);
+    }
+
+    return generatedKey;
+}*/
+
+
+
+std::vector<uint8_t> generateKey(const std::vector<uint8_t>& key, int length) {
+    // return generateKey(key, polynomial, length);
+    return generateKeyAppending(key, polynomial, length);
+}
+
+
+/// оптимизированная версия
+
+uint8_t calculateFeedbackBit(const std::vector<uint8_t>& reg, const std::vector<int>& polynomial, int step) {
+    uint8_t feedback = 0;
+    for (int tap : polynomial) {
+        if (tap + step > reg.size())
+            continue;
+        feedback ^= reg[30 - tap + step];
+    }
+    return feedback;
+}
+
+std::vector<uint8_t> generateKeyAppending(const std::vector<uint8_t>& initialKey, const std::vector<int>& polynomial, int outputLength) {
+    std::vector<uint8_t> generatedKey(outputLength);
+
+    for (int i = 0; i < initialKey.size(); ++i)
+        generatedKey[i] = initialKey[i];
+
+    for (int i = initialKey.size(); i < outputLength; ++i) {
+        generatedKey[i] = calculateFeedbackBit(generatedKey, polynomial, i - 30);
     }
 
     return generatedKey;
--- Key.h
+++ Key.h
@@ -1,4 +1,4 @@
-#ifndef KEY_H
+/*#ifndef KEY_H
 #define KEY_H
 
 #include <cstdint>
@@ -8,10 +8,34 @@ const std::vector<int> polynomial = { 30, 16, 15, 1 };
 
 std::vector<uint8_t> generateKey( std::vector<uint8_t>& key, int length);
 
-uint8_t calculateFeedbackBit(std::vector<uint8_t>& reg, const std::vector<int>& polynomial);
+uint8_t calculateFeedbackBit(const std::vector<uint8_t>& reg, const std::vector<int>& polynomial);
 
 void shiftLeft(std::vector<uint8_t>& reg);
 
 std::vector<uint8_t> generateKeyAppending(std::vector<uint8_t>& initialKey, const std::vector<int>& polynomial, int outputLength);
 
 #endif //KEY_H
+
+
+*/
+
+
+
+#ifndef KEY_H
+#define KEY_H
+
+#include <cstdint>
+#include <vector>
+
+const std::vector<int> polynomial = { 30, 16, 15, 1 };
+
+std::vector<uint8_t> generateKey(const std::vector<uint8_t>& key, int length);
+
+uint8_t getNewBit(std::vector<uint8_t>& key, const std::vector<int>& polynomial);
+
+void leftShift(std::vector<uint8_t>& key);
+
+std::vector<uint8_t> generateKey(const std::vector<uint8_t>& key, const std::vector<int>& polynomial, int length);
+
+std::vector<uint8_t> generateKeyAppending(const std::vector<uint8_t>& initialKey, const std::vector<int>& polynomial, int outputLength);
+#endif 
